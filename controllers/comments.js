0bcc7b59267916d55eeadb359400add31d06c998
Michelle Cheng <cheng.michelley@gmail.com>
2026-05-05 20:06:34 -0700

    Comments Controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,41 @@ const router = require('express').Router()
 const Comment = require('../models/comment')
 const verifyJwt = require('../middleware/verify-jwt')
 
+router.put("/:commentId", verifyJwt, async (req, res) => {
+  try {
+    const updatedComment = await Comment.findOneAndUpdate(
+      { _id: req.params.commentId, author: req.user._id },
+      req.body,
+      { new: true, runValidators: true },
+    ).populate("author");
 
+    if (!updatedComment)
+      return res
+        .status(404)
+        .json({ error: "Comment not found or unauthorized" });
+
+    res.status(200).json(updatedComment);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/:commentId", verifyJwt, async (req, res) => {
+  try {
+    const deletedComment = await Comment.findOneAndDelete({
+      _id: req.params.commentId,
+      author: req.user._id,
+    });
+
+    if (!deletedComment)
+      return res
+        .status(404)
+        .json({ error: "Comment not found or unauthorized" });
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router
