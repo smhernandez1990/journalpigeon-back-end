dac5f8995f173b2d89bbed396cde4075c539b26d
Michelle Cheng <cheng.michelley@gmail.com>
2026-05-13 12:59:53 -0400

    Updated CORS

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const logger = require('morgan');
-const authController = require('./controllers/auth')
-const usersController = require('./controllers/users')
-const postsController = require('./controllers/posts')
-const commentsController = require('./controllers/comments')
-const verifyJwt = require('./middleware/verify-jwt')
-const corsConfig = require('./middleware/corsConfig')
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
 
-require('./db/connection')
+require("./db/connection");
+
+const authController = require("./controllers/auth");
+const usersController = require("./controllers/users");
+const postsController = require("./controllers/posts");
+const commentsController = require("./controllers/comments");
 
 const corsOptions = {
-    origin: 'https://journal-pigeon.netlify.app',
-    optionsSuccessStatus: 200
-}
-
+  origin: "https://journal-pigeon.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(logger('dev'));
+app.options("*", cors(corsOptions));
 
-// Routes
-app.use('/auth', authController)
-app.use('/users', usersController)
-app.use('/posts', postsController)
-app.use("/posts/:postId/comments", commentsController);
+app.use(express.json());
+app.use(logger("dev"));
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.listen(3000, () => {
-    console.log('Microphone Check One Two What Is This');
+app.use("/auth", authController);
+app.use("/users", usersController);
+app.use("/posts/:postId/comments", commentsController);
+app.use("/posts", postsController);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Microphone Check One Two What Is This on port ${PORT}`);
 });
